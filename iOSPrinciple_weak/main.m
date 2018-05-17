fe42a734ed36858064453080949e7d4d0c5d30cc
Steven <reversescale@icloud.com>
2018-05-17 11:38:58 +0800

    🎉:Initial commit.

---
+++ iOSPrinciple_weak/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSPrinciple_weak
+//
+//  Created by WhatsXie on 2018/5/17.
+//  Copyright © 2018年 WhatsXie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
